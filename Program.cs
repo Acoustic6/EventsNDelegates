00e76deb6dca69d7b8e28fefa3cd9add58c13e9e
Acoustic6 <dorozhko.dmitry@gmail.com>
2017-11-26 13:12:49 +0300

    it's sad without delegates

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EventsNDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> result = GetAllNumbersLessThanFive(new[] { 2, 7, 6, 9, 4, 6, 2 });
+            
+            foreach (int n in result)
+            {
+                Console.WriteLine(n);
+            }
+            Console.ReadKey();
+        }
+
+        static IEnumerable<int> GetAllNumbersLessThanFive(IEnumerable<int> numbers)
+        {
+            foreach (int number in numbers)
+            {
+                if (number < 5)
+                    yield return number;
+            }
+        }
+
+        static IEnumerable<int> GetAllNumbersLessThanTen(IEnumerable<int> numbers)
+        {
+            foreach (int number in numbers)
+            {
+                if (number < 10)
+                    yield return number;
+            }
+        }
+
+        static IEnumerable<int> GetAllNumbersGreaterThanEight(IEnumerable<int> numbers)
+        {
+            foreach (int number in numbers)
+            {
+                if (number > 8)
+                    yield return number;
+            }
+        }
+    }
+}
